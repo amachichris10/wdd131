ac93caade43c667b812c99fa36563412b76f5510
amachichris10 <amachi.chris.10@gmail.com>
2026-07-02 16:00:46 -0500

    css updated and html improved

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 const year = document.querySelector("#current-year");
 const today = new Date();
 
-year.innerHTML = today.getFullYear();
+year.innerHTML = `&copy; ${today.getFullYear()}`;
 
-document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
